077f1fd9291b20ae310a9cc12ec91b6ea4d62b31
Sky <kazisakiya11@outlook.com>
2025-10-17 22:19:48 +0600

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,15 @@
+name1 = "ava"
+name2 = "josh"
+name3 = "sophie"
+name4 = "mason"
+name5 = "olivia"
+birthday1 = "22nd/jan/2011"
+birthday2 = "15th/feb/2010"
+birthday3 = "21th/apr/2011"
+birthday4 = "30th/mar/2011"
+birthday5 = "5th/may/2011"
+print(name1 + "'s birthday is " + birthday1)
+print(name2 + "'s birthday is " + birthday2)
+print(name3 + "'s birthday is " + birthday3)
+print(name4 + "'s birthday is " + birthday4)
+print(name5 + "'s birthday is " + birthday5)
